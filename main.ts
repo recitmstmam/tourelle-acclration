078fe0276d208363ea9d55862281e90c22e79fc4
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-05-13 20:55:42 -0400

    Deux blocs toujours pour mouvement des deux moteurs en même temps

--- main.ts
+++ main.ts
@@ -3,13 +3,7 @@ let angle1 = 90
 servos.P0.setAngle(angle_0)
 servos.P1.setAngle(angle1)
 basic.forever(function () {
-    if (input.acceleration(Dimension.X) >= 150 && angle1 < 175) {
-        angle1 += 2.5
-        servos.P1.setAngle(angle1)
-    } else if (input.acceleration(Dimension.X) <= -150 && angle1 > 5) {
-        angle1 += -2.5
-        servos.P1.setAngle(angle1)
-    } else if (input.acceleration(Dimension.Y) >= 150 && angle_0 < 175) {
+    if (input.acceleration(Dimension.Y) >= 150 && angle_0 < 175) {
         angle_0 += 2.5
         servos.P0.setAngle(angle_0)
     } else if (input.acceleration(Dimension.Y) <= -150 && angle_0 > 5) {
@@ -17,6 +11,16 @@ basic.forever(function () {
         servos.P0.setAngle(angle_0)
     } else {
         servos.P0.stop()
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) >= 150 && angle1 < 175) {
+        angle1 += 2.5
+        servos.P1.setAngle(angle1)
+    } else if (input.acceleration(Dimension.X) <= -150 && angle1 > 5) {
+        angle1 += -2.5
+        servos.P1.setAngle(angle1)
+    } else {
         servos.P1.stop()
     }
 })
